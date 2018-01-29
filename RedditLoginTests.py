5072f5c3f41deafacf20b8ab8662104733244850
rogeiio <rogeiio>
2018-01-29 12:12:34 -0500

    Remove hardcoded working username. Add additional comments

--- RedditLoginTests.py
+++ RedditLoginTests.py
@@ -1,16 +1,27 @@
+# import needed webdriver from selenium
 from selenium import webdriver
+
+# import keys to let the driver "Press" the Return key
 from selenium.webdriver.common.keys import Keys
+
+# import time to allow driver to sleep for a few seconds after attempting to login
 import time
 
+# provide working username and password when running
+USER_NAME = ''
+PASSWORD = ''
 driver = webdriver.Chrome()
 
 
+# Go to the website and make sure it is the correct site
 def goToSite():
     driver.get("https://www.reddit.com/")
     assert "reddit" in driver.title
 
 
+# attempt to login to the site using the provided user and password
 def attemptLogin(loginUser, loginPwd):
+    # no need to try to log in if the user is already logged in
     if isLoggedIn():
         return
     elem = driver.find_element_by_name('user')
@@ -20,9 +31,13 @@ def attemptLogin(loginUser, loginPwd):
     elem.send_keys(Keys.RETURN)
 
 
+# attempt to logout of the site
 def attemptLogout():
+    # no need to try to logout if the user is not logged in
     if not isLoggedIn():
         return
+
+    # submit logout form if it exists
     try:
         elem = driver.find_element_by_class_name('logout')
         elem.submit()
@@ -52,7 +67,7 @@ def loginFail():
 def loginSuccess():
     goToSite()
     assert isLoggedIn() == False
-    attemptLogin('rogelioTesting', 'TestPassword')
+    attemptLogin(USER_NAME, PASSWORD)
     time.sleep(2)
     assert isLoggedIn() == True
     attemptLogout()
